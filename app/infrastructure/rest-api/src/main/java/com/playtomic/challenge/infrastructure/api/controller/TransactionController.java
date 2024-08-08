08ce6a38c7144b24400c80c1a4b99962d442b8b5
Alex Vall <alex.vall@wefox.com>
2024-08-08 07:14:37 +0200

    PlayTomic Challenge - Refund transaction

--- app/infrastructure/rest-api/src/main/java/com/playtomic/challenge/infrastructure/api/controller/TransactionController.java
+++ app/infrastructure/rest-api/src/main/java/com/playtomic/challenge/infrastructure/api/controller/TransactionController.java
@@ -4,9 +4,12 @@ import com.playtomic.challenge.api.lib.controller.TransactionV1Api;
 import com.playtomic.challenge.api.lib.model.TransactionCreateRequestDto;
 import com.playtomic.challenge.api.lib.model.TransactionResponseDto;
 import com.playtomic.challenge.application.port.input.CreateChargeTransactionUseCase;
+import com.playtomic.challenge.application.port.input.CreateRefundTransactionUseCase;
+import com.playtomic.challenge.domain.model.TransactionType;
 import com.playtomic.challenge.infrastructure.api.mapper.TransactionDtoMapper;
 import com.playtomic.challenge.infrastructure.api.presenter.UseCaseExecutorPort;
-import lombok.RequiredArgsConstructor;
+import java.util.Map;
+import java.util.function.Function;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,21 +17,51 @@ import org.springframework.web.bind.annotation.RestController;
  * Controller from OpenApi definition
  */
 @RestController
-@RequiredArgsConstructor
 public class TransactionController implements TransactionV1Api, UseCaseExecutorPort {
-  private final CreateChargeTransactionUseCase useCase;
+  private final CreateChargeTransactionUseCase useCaseCharge;
+  private final CreateRefundTransactionUseCase useCaseRefund;
   private final TransactionDtoMapper mapper;
+  private final Map<TransactionType,
+      Function<TransactionCreateRequestDto, ResponseEntity<TransactionResponseDto>>> useCases;
 
+
+  public TransactionController(
+      CreateChargeTransactionUseCase useCaseCharge,
+      CreateRefundTransactionUseCase useCaseRefund,
+      TransactionDtoMapper mapper) {
+
+    this.mapper = mapper;
+    this.useCaseCharge = useCaseCharge;
+    this.useCaseRefund = useCaseRefund;
+    this.useCases  = Map.of(
+        TransactionType.TOPUP, dto -> {
+          return functional(
+              CreateChargeTransactionUseCase.InputValues
+                  .builder()
+                  .transaction(mapper.toDomain(dto))
+                  .build(),
+              useCaseCharge,
+              output -> ResponseEntity.status(201).body(mapper.toDto(output.getTransaction())
+          ));
+        },
+        TransactionType.REFUND, dto -> {
+          return functional(
+              CreateChargeTransactionUseCase.InputValues
+                  .builder()
+                  .transaction(mapper.toDomain(dto))
+                  .build(),
+              useCaseCharge,
+              output -> ResponseEntity.status(201).body(mapper.toDto(output.getTransaction())
+          ));
+        }
+    );
+
+  }
   @Override
   public ResponseEntity<TransactionResponseDto> createTransaction(
       TransactionCreateRequestDto transactionCreateRequestDto) {
-    return functional(
-        CreateChargeTransactionUseCase.InputValues
-            .builder()
-            .transaction(mapper.toDomain(transactionCreateRequestDto))
-            .build(),
-        useCase,
-        output -> ResponseEntity.status(201).body(mapper.toDto(output.getTransaction()))
-    );
+    return useCases.get(TransactionType
+        .valueOf(transactionCreateRequestDto.getType().toString()))
+        .apply(transactionCreateRequestDto);
   }
 }
